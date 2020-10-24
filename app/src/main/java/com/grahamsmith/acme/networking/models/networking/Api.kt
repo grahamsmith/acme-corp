68ae9eed4dd6c1a8e9a3bc0d861082432e0e6219
Graham Smith <graham@wiseman-designs.com>
2020-10-24 23:22:04 +0100

    Update API Definitions to return Retrofit encapsulated objects

--- app/src/main/java/com/grahamsmith/acme/networking/models/networking/Api.kt
+++ app/src/main/java/com/grahamsmith/acme/networking/models/networking/Api.kt
@@ -3,13 +3,14 @@ package com.grahamsmith.acme.networking.models.networking
 import com.grahamsmith.acme.networking.models.networking.login.LoginRequest
 import com.grahamsmith.acme.networking.models.networking.login.LoginResponse
 import com.grahamsmith.acme.networking.models.networking.profiles.ProfilesResponse
+import retrofit2.Response
 import retrofit2.http.*
 
 interface Api {
 
     @POST("https://ho0lwtvpzh.execute-api.us-east-1.amazonaws.com/DummyLogin")
-    suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
+    suspend fun login(@Body loginRequest: LoginRequest): Response<LoginResponse>
 
     @GET("https://ypznjlmial.execute-api.us-east-1.amazonaws.com/DummyProfileList")
-    suspend fun getProfiles(@Header("authorization") authToken: String): ProfilesResponse
+    suspend fun getProfiles(@Header("authorization") authToken: String): Response<ProfilesResponse>
 }
